720f3bb45e264d4cf67ce00dea38e1ca2095d545
LeandroCosma <93363721+LeandroCosma@users.noreply.github.com>
2022-04-04 19:45:03 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log('Hola mundo')
+
+import {User} from './User.js'
+import {Product} from './Product.js'
+
+
+const yo = new User('Leandro', 'Cosma')
+console.log(yo)
+
+const coca = new Product('Coca-Cola', '100')
+console.log(coca)
